25c32e3dac32367d445c9301e26d204b91f6d041
Mike <mike.polis@gmail.com>
2016-11-28 13:46:48 -0600

    update to use WeService as application callback and update the instructions

--- oauth_test_client.rb
+++ oauth_test_client.rb
@@ -1,15 +1,19 @@
 require 'oauth2'
 require 'pry'
 
-callback_url = 'http://localhost:3000/oauth/test_client/callback'
-client_id = '59c5bb1b338be44b459e003bb9191b3198c00bb8bf9fb8720e0fd85b94a3489d'
-client_secret = '7993a3f010ba268a52fb850c263f2eb5ee0affbedfaf184432eb4d786152a47b'
+callback_url = 'http://localhost:3001/auth/wecounsel/callback'
+client_id = '24f58e3b8a0bca3c8f650572bfb0726b6f09c819cc73c1533c935c6b2e213529'
+client_secret = '8c4c50624297675cac4fdd10b0fa4b277c6ae8e30d675853eab06c1a9cc12775'
 
 client = OAuth2::Client.new(client_id, client_secret, :site => 'http://localhost:3000')
 auth_url = client.auth_code.authorize_url(:redirect_uri => callback_url)
-puts "Visit the following URL and copy the 'code' query param in the redirected URL: #{auth_url}"
+puts "Make sure WeCounsel server is running at http://localhost:3000, visit the following URL and copy the 'code' query param in the redirected address line (ignore the load error):"
+puts "#{auth_url}"
 puts "Enter the code param here: "
 code = gets.chomp
 access = client.auth_code.get_token(code, :redirect_uri => callback_url)
 token = access.token
+puts
 puts "Hooray! Here's your oAuth token: #{token}"
+puts "Now you can for example open this link: "
+puts "http://api.lvh.me:3000/v1/users/225?access_token=#{token} (substitute 225 for existing user_id)"
