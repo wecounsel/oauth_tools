e6116b3e7e1c27ff3c21ffc62f9f43d43e36b755
Mike <mike.polis@gmail.com>
2017-05-16 07:57:44 -0600

    improving documentation adding support for edge

--- helpers.rb
+++ helpers.rb
@@ -1,5 +1,11 @@
 # this is just for printing example of protected url that can be accessed with token
 def get_example_api_url(token)
-  ENV['ENV'] == 'staging' ? "https://staging.wecounsel.com/v1/users/225?access_token=#{token}" :
+  case ENV['ENV']
+    when 'staging'
+      "https://staging.wecounsel.com/v1/users/225?access_token=#{token}"
+    when 'edge'
+      "https://test-pp.wecounsel.com/v1/users/225?access_token=#{token}"
+    else
       "http://localhost:3000/v1/users/225?access_token=#{token}"
+  end
 end
--- oauth_test_client.rb
+++ oauth_test_client.rb
@@ -9,10 +9,10 @@ Settings.load!("secrets.yml", :env => ENV['ENV'] || 'development')
 client = OAuth2::Client.new(Settings.client_id, Settings.client_secret, :site => Settings.provider_url)
 auth_url = client.auth_code.authorize_url(:redirect_uri => Settings.callback_url)
 
-if ENV['ENV'] == 'staging'
+if ['staging', 'edge'].include?(ENV['ENV'])
   puts "Visit the following URL, sign-in with your Wecounsel account, and copy the 'code' query param in the redirected address line (ignore the page error):"
 else
-  puts "1) Visit #{Settings.provider_url}/oauth/applications , add a new application, and copy the the values of Application ID, Secret and Callback URL to helpers.rb "
+  puts "1) Visit #{Settings.provider_url}/oauth/applications , add a new application, and copy the the values of Application ID, Secret and Callback URL to secrets.yml"
   puts "2) Make sure WeCounsel server is running at #{Settings.provider_url}, visit the following URL, sign-in with your Wecounsel account, and copy the 'code' query param in the redirected address line (ignore the page error):"
 end
 
@@ -22,5 +22,5 @@ code = gets.chomp
 access = client.auth_code.get_token(code, :redirect_uri => Settings.callback_url)
 puts
 puts "Hooray! Here's your oAuth access token: #{access.token}"
-puts "Now you can for example open this link: #{get_example_api_url(access.token)} (substitute 225 for existing Wecounsel user_id)"
+puts "Now you can for example access this link: #{get_example_api_url(access.token)} (substitute 225 for existing Wecounsel user_id)"
 puts "And here's your refresh token: #{access.refresh_token} . You can use it to get a new access token, when the existing one expires. "
